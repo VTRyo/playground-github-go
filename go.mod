ab5edb8e45362312a85065db9e9f779688a9f2a1
VTRyo <vtryo.work@gmail.com>
2022-07-20 23:29:53 +0900

    get issues

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/VTRyo/playground-github-go
 go 1.18
 
 require (
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-github v17.0.0+incompatible // indirect
 	github.com/google/go-github/v45 v45.2.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/oauth2 v0.0.0-20220718184931-c8730f7fcb92 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,17 +3,49 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/google/go-github/github"
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
 )
 
-func getPublicOrg() {
+func getPublicReposByOrg() {
 	client := github.NewClient(nil)
 
-	opt := &github.RepositoryListByOrgOptions{Type: "public"}
+	opt := &github.RepositoryListByOrgOptions{
+		ListOptions: github.ListOptions{PerPage: 1}, // pagenation
+		Type:        "public",                       // org type
+	}
+
 	repos, _, err := client.Repositories.ListByOrg(context.Background(), "moneyforward", opt)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(repos)
+}
+
+func main() {
+	envErr := godotenv.Load(fmt.Sprintf(".env"))
+	if envErr != nil {
+		fmt.Printf("環境変数を読み込めませんでした: %v", envErr)
+	}
+
+	repoName := ""
+	ownerName := "VTRyo"
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_ACCESS_TOKEN")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	issues, _, err := client.Issues.ListByRepo(ctx, ownerName, repoName, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(issues)
+	getPublicReposByOrg()
 }
